bd7df9c706ef58b37e7f6e94951c73b0b375ffcf
huangyongsup <huangyongsup@icloud.com>
2019-03-23 20:53:02 +0800

    nothing

--- fore-end/index.js
+++ fore-end/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <HashRouter>
-    </HashRouter>
-    ,document.getElementById('root')
+  <HashRouter>
+  </HashRouter>
+  ,document.getElementById('root')
 );
