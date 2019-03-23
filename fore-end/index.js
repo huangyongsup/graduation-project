5e4d82aa3eeead7c3545e91da1850df881496b2a
huangyongsup <huangyongsup@icloud.com>
2019-03-23 16:52:48 +0800

    项目初始化

---
+++ fore-end/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
+
+ReactDOM.render(
+    <HashRouter>
+    </HashRouter>
+    ,document.getElementById('root')
+);
